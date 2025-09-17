ca327e8a2038f0a46d72327a0527e5ee646e9a1d
John <johnjayo1007@gmail.com>
2025-09-17 17:43:02 -0500

    Practica de git

---
+++ practica sem4/practica python  sem4/practica_python__sem4.py
@@ -0,0 +1,94 @@
+﻿from importlib.metadata import packages_distributions
+from xml.etree.ElementTree import PI
+
+
+def ejer1():
+    edad =      int(input("Ingrese su edad: "))
+    if edad > 130 or edad < 0:
+        print("Esa edad no es valida")
+    elif edad < 18:
+        print ("Usted es Menor de edad")
+    elif 18 <= edad <= 64:
+        print ("Usted esta Adulto")
+    else:
+        print ("Usted es Adulto Mayor")
+
+def ejer2():
+    año = int(input("Ingrese el anio: "))
+
+    if año % 4 == 0:
+       if año % 100 == 0:
+          if año % 400 == 0:
+            print ("Es año bisiesto")
+          else:
+              print("No es Año bisiesto")
+       else:
+           print("Es Año bisiesto")
+    else:
+        print("No es Año bisiesto")
+
+    if año % 2 == 0:
+                print ("Es un año par")
+    else:
+                print("Es un año impar")
+
+                 
+def ejer3():
+    monto = float(input("Ingrese un monto en Soles (PEN): "))
+    print("¿Elige una opcion?")
+    print(" (1) USD ")
+    print(" (2) EUR ")
+    print( " (3) Salir ")
+
+    opcion = int(input("Eliga una opcion:  "))
+
+    USD = monto/3.75
+    EUR = monto/4.05
+
+    match opcion:
+        case 1:
+            print(f"En dolares es: {USD:.2f}")
+        case 2:
+            print(f"En Euros es: {EUR:.2f}")
+        case 3:
+            print("Saliendo del programa")
+        case _ :
+            print("Opcion no valida")   
+
+
+import math 
+
+def ejer4():
+    print("¿Que quieres realizar?")
+    print("------ Seleccion de Areas ------")
+    print("(1)Area del Cuadrado")
+    print("(2)Area del Rectangulo")
+    print("(3)Area del Triangulo")
+    print("(4)Area del Circulo")
+    opcion = int(input("Eliga una opcion:  "))
+
+    match opcion:
+        case 1:
+            lado = float(input("Ingrese el Lado del cuadrado: "))
+            areacua = lado*lado 
+            print(f"El Area del Cuadrado es: {areacua:.2f}")
+        case 2:
+            base = float(input("Ingrese la base del rectangulo: "))
+            alt = float(input("Ingrese la altura del rectangulo: "))
+            arearect = base*alt
+            print(f"El Area del rectangulo es: {arearect:.2f}")
+        case 3:
+            base = float(input("Ingrese la base del triangulo: "))
+            alt = float(input("Ingrese la altura del triangulo: "))
+            areatria = (base*alt)/2
+            print(f"El Area del triangulo es: {areatria:.2f}")
+        case 4:
+            radio = float(input("Ingrese el Radio del Triangulo: "))
+            areacirc = math.pi * (radio ** 2)
+            print(f"El area del Circulo es: {areacirc:.2f}")
+        case _ :
+            print("La opcion no es valida")
+
+
+
+ejer4()
---
+++ practica sem4/practica sem4/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_sem4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejer5();
+            Console.ReadKey();
+
+        }
+        static void ejer2()
+        {
+            Console.Write("Ingrese un año: ");
+            int año = int.Parse(Console.ReadLine());
+
+            if (año % 4 == 0)
+                if (año % 100 == 0)
+                    if (año % 400 == 0) ;
+                    else
+                        Console.WriteLine("No es Año bisiesto");
+                else
+                    Console.WriteLine("Es Año bisiesto");
+            else
+                Console.WriteLine("No es Año bisiesto");
+            if (año % 2 == 0)
+                Console.WriteLine("Es un Año Par");
+            else
+                Console.WriteLine("Es un Año Impar");
+        }
+        static void ejer3()
+        {
+            Console.Write("Ingrese el monto en Soles(PEN): ");
+            double monto = double.Parse(Console.ReadLine());
+            Console.WriteLine("\n¿A que quiere Convertir?: ");
+            Console.WriteLine(" (1) USD ");
+            Console.WriteLine(" (2) EUR ");
+            Console.WriteLine(" Elige una opcion:");
+            int opcion = int.Parse(Console.ReadLine());
+
+            double USD = (monto / 3.75);
+            double EUR = (monto / 4.05); 
+
+            switch (opcion)
+            {
+                case 1:
+                Console.WriteLine("En dolares son:  " + USD.ToString("0.00") );
+                    break;
+                case 2:
+                    Console.WriteLine("En Euros son: " + EUR.ToString("0.00"));
+                    break;
+                default:
+                    Console.WriteLine("Datos no encontrado");
+                    break;
+
+            }
+            Console.ReadKey();
+
+
+        }
+
+        static void ejer4()
+        {
+            Console.WriteLine("Selecciona la Accion que quiere realizar");
+            Console.WriteLine("------ Calculos de areas ------ ");
+            Console.WriteLine("(1)Area del cuadrado:");
+            Console.WriteLine("(2)Area del Rectangulo: ");
+            Console.WriteLine("(3)Area del Triangulo: ");
+            Console.WriteLine("(4)Area del Circulo: ");
+            Console.Write("Elige una Opcion:  ");
+
+            int opcion = int.Parse(Console.ReadLine());
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.Write("Ingrese el lado del Cuadrado: ");
+                    double lado = double.Parse(Console.ReadLine());
+                    double areacua = lado * lado;
+                    Console.WriteLine("El Area del Cuadrado es: " + areacua);
+                    break;
+                case 2:
+                    Console.Write("Ingrese la base del rectangulo: ");
+                    double barec = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura del rectangulo: ");
+                    double alrect = double.Parse(Console.ReadLine());
+                    double arearect = barec * alrect;
+                    Console.WriteLine("El area del rectangulo es: " + arearect);
+                    break;
+                case 3:
+                    Console.Write("Ingrese la base del triangulo: ");
+                    double batria = double.Parse(Console.ReadLine());
+                    Console.Write("Ingrese la altura del triangulo: ");
+                    double altria = double.Parse(Console.ReadLine());
+                    double areatria = (batria * altria) / 2;
+                    Console.WriteLine("El area del triangulo es: " + areatria);
+                    break;
+                case 4:
+                    Console.Write("Ingrese el radio del circulo: ");
+                    double rad = double.Parse(Console.ReadLine());
+                    double areacirc = Math.PI * Math.Pow(rad, 2);
+                    Console.WriteLine("El Area del Circulo es: " + areacirc.ToString("0.00"));
+                    break;
+                default:
+                    Console.WriteLine("Valor ingresado incorrecto.");
+                    break;
+             
+            }
+        }
+
+        static void ejer5()
+        {
+            Console.Write("Ingrese su 1ra nota: ");
+            int nota1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese su 2da nota: ");
+            int nota2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese su 3ra nota: ");
+            int nota3 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese su 4ta nota: ");
+            int nota4 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese su 5ta nota: ");
+            int nota5 = int.Parse(Console.ReadLine());
+
+            int notafinal = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+            
+            Console.WriteLine("SU NOTA FINAL ES: " + notafinal);
+        }
+    }
+ }
